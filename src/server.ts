48df9fbb6d14f9c829479a69474efb1b86dd8442
Anderson Luís Silva <andersonluis87@gmail.com>
2020-08-10 08:54:34 -0300

    Configuring tsconfig-paths and basic tsconfig.json

---
+++ src/server.ts
@@ -0,0 +1,15 @@
+import express from 'express';
+
+const app = express();
+
+app.use(express.json())
+
+app.get('/', (request, response) => {
+    return response.json({ message: 'Hello World' });
+});
+
+const port = process.env.PORT || 3333;
+
+app.listen(port, () => {
+    console.log(`🚀 Server is running on port ${port}...`)
+}); 
